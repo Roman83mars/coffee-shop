20103e993b835325e354a1c320a7155ae946f7d4
Roman Amb <roman83mars@gmail.com>
2024-05-08 23:44:02 +0300

    lesson 10 complete

---
+++ src/validators/checked.js
@@ -0,0 +1,3 @@
+export const checked = (value) => {
+    return value
+}
--- src/views/ContactUsView.vue
+++ src/views/ContactUsView.vue
@@ -23,7 +23,7 @@
 
             <form @submit.prevent="submit" action="#" class="mt-5">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
@@ -31,16 +31,19 @@
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.name"
+                    v-model="v$.name.$model"
                     type="text"
                     class="form-control"
                     id="name-input"
                   />
+                  <span v-for="error in v$.name.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
@@ -48,30 +51,36 @@
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.email"
+                    v-model="v$.email.$model"
                     type="email"
                     class="form-control"
                     id="email-input"
                   />
+                  <span v-for="error in v$.email.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.phone"
+                    v-model="v$.phone.$model"
                     type="tel"
                     class="form-control"
                     id="phone-input"
                   />
+                  <span v-for="error in v$.phone.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row textarea">
-                <div class="col col-12 d-flex justify-content-center">
+                <div class="col col-12 d-flex justify-content-start">
                   <label for="pmessage" class="mb-3 mt-3 text-center">
                     Your message
                     <span style="color: red">*</span>
@@ -79,14 +88,34 @@
                 </div>
                 <div class="col col-12">
                   <textarea
-                    v-model="form.message"
+                    v-model="v$.message.$model"
                     class="form-control"
                     name="message"
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
+              </div>
+
+              <div class="form-group row">
+                <div class="col col-12 d-flex align-items-start">
+                  <label for="checkbox" class="mt-3 text-center">
+                    <input
+                      type="checkbox"
+                      name="checkbox"
+                      id="checkbox"
+                      v-model="v$.checkbox.$model"
+                    />
+                    Согласен с договором оферты
+                  </label>
+                </div>
+                <span v-for="error in v$.checkbox.$errors" :key="error.$uid">
+                  {{ error.$message }}
+                </span>
               </div>
 
               <div class="row">
@@ -104,21 +133,45 @@
 
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, maxLength } from "@vuelidate/validators";
+import { helpers } from "@vuelidate/validators";
+import { minLength } from "../validators/minLength";
+import { checked } from "@/validators/checked";
 export default {
-  components: { NavBarComponent },
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data() {
     return {
-      form: {
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+      checkbox: true,
+    };
+  },
+  components: { NavBarComponent },
+  validations() {
+    return {
+      name: { required },
+      email: { required, email },
+      phone: {},
+      message: {
+        required,
+        maxLength: maxLength(20),
+        minLength: helpers.withMessage("this value min 5", minLength),
+      },
+      checkbox: {
+        required,
+        checked: helpers.withMessage("Необходимо согласие", checked),
       },
     };
   },
   methods: {
-    submit() {
-      console.log(this.form);
+    async submit() {
+      const isFormCorrect = await this.v$.$validate();
+      if (!isFormCorrect) return;
     },
   },
 };
