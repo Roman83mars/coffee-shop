090b7f342fe744ce56d54479529b98b0ffe515ee
Roman Amb <roman83mars@gmail.com>
2024-05-11 00:06:01 +0300

    lesson 12 complete

--- src/store/coffee.js
+++ src/store/coffee.js
@@ -1,59 +1,30 @@
 const coffee = {
     state: {
-        coffee: [
-            {
-                id: 0,
-                name: "Solimo Coffee Beans 2kg",
-                country: "Brazil",
-                price: 10.73,
-                image: "good-1.jpg",
-            },
-            {
-                id: 1,
-                name: "Presto Coffee Beans 1kg",
-                country: "Brazil",
-                price: 15.99,
-                image: "good-1.jpg",
-            },
-            {
-                id: 2,
-                name: "AROMISTICO Coffee 1kg",
-                country: "Brazil",
-                price: 6.99,
-                image: "good-1.jpg",
-            },
-            {
-                id: 3,
-                name: "Solimo Coffee Beans 2kg",
-                country: "Brazil",
-                price: 10.73,
-                image: "good-1.jpg",
-            },
-            {
-                id: 4,
-                name: "Solimo Coffee Beans 2kg",
-                country: "Brazil",
-                price: 10.73,
-                image: "good-1.jpg",
-            },
-            {
-                id: 5,
-                name: "Solimo Coffee Beans 2kg",
-                country: "Brazil",
-                price: 10.73,
-                image: "good-1.jpg",
-            },
-        ],
+        coffee: [],
+        searchValue: '',
+        sortValue: '',
     },
     mutations: {
         setCoffeeData(state, data) {
             state.coffee = data
-        }
+        },
+        setSearchValue(state, value) {
+            state.searchValue = value
+        },
+        setSortValue(state, value) {
+            state.sortValue = value
+        },
     },
     actions: {
         setCoffeeData({ commit }, data) {
             commit('setCoffeeData', data);
-        }
+        },
+        setSearchValue({ commit }, value) {
+            commit('setSearchValue', value);
+        },
+        setSortValue({ commit }, value) {
+            commit('setSortValue', value);
+        },
     },
     getters: {
         getCoffee(state) {
@@ -63,7 +34,10 @@ const coffee = {
             return (id) => {
                 return state.coffee.find((card) => card.id === +id)
             }
-        }
+        },
+        getSearchValue(state) {
+            return state.searchValue
+        },
     }
 }
 export default coffee
